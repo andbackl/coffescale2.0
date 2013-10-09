57f4af3612549910b69728b87697fc87a263d11c
Oskar Dillen <dillenmeister@gmail.com>
2013-10-09 07:13:48 +0200

    Cleaning up of unused stuff

--- Coffee.Web/Controllers/CoffeeApiController.cs
+++ Coffee.Web/Controllers/CoffeeApiController.cs
@@ -1,35 +1,16 @@
 ﻿using System;
 using System.Web.Http;
 using Coffee.Web.Hubs;
+using Coffee.Web.Models;
 using Microsoft.AspNet.SignalR;
 
 namespace Coffee.Web.Controllers
 {
     public class CoffeeApiController : ApiController
     {
-        public void Post(CoffeeDataChangedEvent item)
-        {
-            GlobalHost.ConnectionManager.GetHubContext<CoffeeHub>().Clients.All.CoffeeDataChanged(item);
-        }
-
         public void Put(CoffeeDataTick tick)
         {
             GlobalHost.ConnectionManager.GetHubContext<CoffeeHub>().Clients.All.CoffeeDataTick(tick);
         }
-    }
-
-    public class CoffeeDataTick
-    {
-        public string StateName { get; set; }
-        public long ElapsedTime { get; set; }
-        public int Weight { get; set; }
-    }
-
-    public class CoffeeDataChangedEvent
-    {
-        public string SerialNumber { get; set; }
-        public DateTime Date { get; set; }
-        public int Status { get; set; }
-        public int Weight { get; set; }      
-    }
+    }        
 }
---
+++ Coffee.Web/Models/CoffeeDataTick.cs
@@ -0,0 +1,9 @@
+﻿namespace Coffee.Web.Models
+{
+    public class CoffeeDataTick
+    {
+        public string StateName { get; set; }
+        public long ElapsedTime { get; set; }
+        public int Weight { get; set; }
+    }
+}

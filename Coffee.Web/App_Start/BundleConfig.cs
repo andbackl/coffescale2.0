5d44441cf165478b3a02789a8ab6e2809ebadebb
Oskar Dillen <dillenmeister@gmail.com>
2013-10-10 06:31:05 +0200

    Script and CSS bundling and removed unused files

---
+++ Coffee.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Optimization;
+
+namespace Coffee.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/css/bundle").Include(
+                "~/Content/bootstrap/bootstrap.css", 
+                "~/Content/bootstrap/site.css"));
+
+            bundles.Add(new ScriptBundle("~/js/bundle").Include(
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/jquery.signalR-2.0.0-rc1.js"));            
+        }
+    }
+}
